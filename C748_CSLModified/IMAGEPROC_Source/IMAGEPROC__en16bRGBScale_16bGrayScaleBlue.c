e4751ab2337d76a9fed9cf7a7def7f1ced6e97d2
InDeviceMex <indevicemex@gmail.com>
2019-08-31 00:35:19 -0500

    [C6748_CSLModified] Optimized

--- C748_CSLModified/IMAGEPROC_Source/IMAGEPROC__en16bRGBScale_16bGrayScaleBlue.c
+++ C748_CSLModified/IMAGEPROC_Source/IMAGEPROC__en16bRGBScale_16bGrayScaleBlue.c
@@ -11,6 +11,7 @@
 
 #include <ImageProcessing.h>
 
+#define OPT (28)
 IMAGPROC_nStatus IMAGEPROC__en16bRGBScale_16bGrayScaleBlue(LCDC_TFT_TypeDef* psLayerSource, LCDC_TFT_TypeDef* psLayerDest,LCDC_DIMENSIONS_TypeDef sDim)
 {
     LCDC_TFT_TypeDef sLayer;
@@ -30,7 +31,8 @@ IMAGPROC_nStatus IMAGEPROC__en16bRGBScale_16bGrayScaleBlue(LCDC_TFT_TypeDef* psL
     uint16_t u16DimWidth=sDim.width;
     uint16_t u16DimHeight=sDim.height;
     uint16_t u16Aux=0;
-    uint16_t u16Blue=0;
+
+    uint8_t u8Mod=0;
 
     if((psLayerSource->variableType != VARIABLETYPE_enUSHORT) || (psLayerDest->variableType != VARIABLETYPE_enUSHORT))
             return IMAGPROC_enERROR;
@@ -56,9 +58,12 @@ IMAGPROC_nStatus IMAGEPROC__en16bRGBScale_16bGrayScaleBlue(LCDC_TFT_TypeDef* psL
         u16DimHeight= u32LayerDestHeightTotal- u16DimY1;
 
     Cache__vWbInvL2 (psLayerSource->layerDataAddress, psLayerSource->layerWidthTotal*psLayerSource->layerHeightTotal*2);
+    u8Mod=(u16DimWidth*u16DimHeight)%OPT;
+    if(u8Mod)
+        u8Mod=OPT-u8Mod;
 
-    uint16_t* restrict pu16LayerSource =(uint16_t *) memalign(1024*1024,sizeof(uint16_t)*u16DimWidth*u16DimHeight+32);
-    uint16_t* restrict pu16LayerDest =(uint16_t *) memalign(1024*1024,sizeof(uint16_t)*u16DimWidth*u16DimHeight+32);
+    uint16_t* restrict pu16LayerSource =(uint16_t *) memalign(1024*1024,sizeof(uint16_t)*u16DimWidth*u16DimHeight+u8Mod);
+    uint16_t* restrict pu16LayerDest =(uint16_t *) memalign(1024*1024,sizeof(uint16_t)*u16DimWidth*u16DimHeight+u8Mod);
 
     uint16_t* restrict pu16LayerSourceInitial =pu16LayerSource;
     uint16_t* restrict pu16LayerDestInitial =pu16LayerDest;
@@ -82,16 +87,13 @@ IMAGPROC_nStatus IMAGEPROC__en16bRGBScale_16bGrayScaleBlue(LCDC_TFT_TypeDef* psL
     _nassert ((int)(pu16LayerDest) % 8 == 0);
 
 
-    #pragma UNROLL(16)
-    #pragma MUST_ITERATE (16,,16)
-    for(s32Index=0;s32Index<u16DimHeight*u16DimWidth;s32Index++)
+    #pragma UNROLL(OPT)
+    #pragma MUST_ITERATE (OPT,,OPT)
+    for(s32Index=0;s32Index<(u16DimHeight*u16DimWidth)+u8Mod;s32Index++)
     {
         u16Aux=*((uint16_t*)pu16LayerSource)&0x001F;
+        *((uint16_t*)pu16LayerDest)=(u16Aux<<11)+ (u16Aux<<6)+u16Aux;
         pu16LayerSource++;
-        u16Blue=u16Aux;
-        u16Blue|=(u16Aux<<6);
-        u16Blue+=(u16Aux<<11);
-        *((uint16_t*)pu16LayerDest)= u16Blue;
         pu16LayerDest++;
     }
 
